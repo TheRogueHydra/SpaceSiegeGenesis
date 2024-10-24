ddfb6bbda6dcd527adb36643441fe9eed20616cb
TheRogueHydra <maheshan.peiris@gmail.com>
2024-10-24 17:00:14 +0530

    Reset project progress.

--- core/src/main/java/com/theroguehydra/GameScreen.java
+++ core/src/main/java/com/theroguehydra/GameScreen.java
@@ -1,6 +1,7 @@
 package com.theroguehydra;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -8,18 +9,14 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import java.util.ArrayList;
 
 public class GameScreen extends ApplicationAdapter {
 
     private OrthographicCamera gameCam;
     private Viewport gameViewport;
     SpriteBatch batch;
-    Texture bgTexture;
-    Texture gameOverText;
 
-    Player player;
-    ArrayList<Bullet> bullets = new ArrayList<Bullet>();
+    Texture backgroundTexture;
 
     @Override
     public void create() {
@@ -28,54 +25,29 @@ public class GameScreen extends ApplicationAdapter {
         gameCam = new OrthographicCamera();
         gameViewport = new FitViewport(640, 480, gameCam);
 
-        batch = new SpriteBatch();
-        bgTexture = new Texture("bg-placeholder.png");
-        gameOverText = new Texture("ui/gameover.png");
-        player = new Player();
-        player.create();
+        // Initializing objects.
+        this.batch = new SpriteBatch();
+        this.backgroundTexture = new Texture(Gdx.files.internal("bg-placeholder.png"));
 
     }
 
     @Override
     public void render() {
-        
-        // Drawing the scene.
+
+        // Update Logic
+
+
+        // Draw Logic
         ScreenUtils.clear(Color.BLACK);
         batch.begin();
-        batch.draw(bgTexture, 0, 0);
-        player.draw(batch);
-        for(int i=0; i<bullets.size(); i++) {
-            Bullet bullet = bullets.get(i);
-            bullet.draw(batch);
-        }
-        checkGameOver(player, batch);
+        batch.draw(backgroundTexture, 0, 0);
         batch.end();
-
-        // Updating the scene.
-        player.update(bullets);
-        for(int i=0; i<bullets.size(); i++) {
-            Bullet bullet = bullets.get(i);
-            bullet.update();
-        }
 
     }
 
     @Override
     public void resize(int width, int height) {
         gameViewport.update(width, height);
-    }
-
-    public void checkGameOver(Player player, SpriteBatch batch) {
-        if(player.HP <= 0) {
-            drawGameOverScreen(batch);
-            player.texture.dispose();
-            player.isActive = false;
-        }
-    }
-
-    public void drawGameOverScreen(SpriteBatch batch) {
-        ScreenUtils.clear(Color.BLACK);
-        batch.draw(gameOverText, 0, 0);
     }
 
 }
